a9203b5ac9c643f448255c469a4ddefcf8696ce2
Clay Mangum <clay@trinitasaccounting.com>
2024-02-02 15:10:08 -0600

    updated the comments in the code

--- src/index.js
+++ src/index.js
@@ -11,43 +11,40 @@
 
 
 
+
+
+
 document.addEventListener("DOMContentLoaded", (event) => {
   // your code here
   event.preventDefault();
 
-
-
+  //Finding the form that we want to change
   const form = document.getElementById('create-task-form');
-  // console.log(form);
+
   form.addEventListener('submit', (event) => {
     event.preventDefault();
+
+    //Grabbing the input element that is in the form so we can access the newInput.value that is input
     const newInput = document.getElementById('new-task-description');
-    // console.log(newInput.value);
+
+    //this is where we are adding a li element onto the correct form Div
     const myTodosDiv = document.querySelector('#tasks');
     const newLiElement = document.createElement('li')
     const newListItem = myTodosDiv.appendChild(newLiElement);
     newListItem.textContent = newInput.value + ' ';
 
+    //Creating the x button and appending
     const btn = document.createElement('button');
     btn.textContent = 'X'
     newListItem.appendChild(btn)
 
     form.reset();
 
+    //Making the button remove the element
     btn.addEventListener('click', (event) => {
       newListItem.remove();
     })
 
 
   })
-
-  // const newTask = taskInput.value;
-  // console.log(taskInput);
-  // console.log(event);
-  // const submitButton = document.querySelector('#submit');
-
 });
-
-// document.addEventListener('click', (event) => {
-//   console.log(event);
-// })
